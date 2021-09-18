94a4b481172968ca4d283447277849d63f6bc81d
metacringer <metacringer@gmail.com>
2021-09-19 02:59:06 +0300

    fixed environment in GH action

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tar -pczvf package.tar.gz --exclude=".github/*" --exclude=".env" --exclude="LICENSE" --exclude="README.md" --exclude=".gitignore" --exclude="script/*" ./*
-echo "host is ${HOST_DELIVERY}"
-echo ${KEY_DELIVERY} | base64 -d > key.pem
-ssh -i key.pem ec2-user@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
-scp -i key.pem package.tar.gz ec2-user@${HOST_DELIVERY}:~/minecraft/
-ssh -i key.pem ec2-user@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
-rm package.tar.gz key.pem

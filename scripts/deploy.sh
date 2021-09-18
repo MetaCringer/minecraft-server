46a0d1c0ff3e08fa1a2b1b2c88e8d0dca183928c
metacringer <metacringer@gmail.com>
2021-09-19 02:46:07 +0300

    remove duplicates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tar -pczvf package.tar.gz --exclude=".github/*" --exclude=".env" --exclude="LICENSE" --exclude="README.md" --exclude=".gitignore" --exclude="script/*" ./*
+echo "host is ${HOST_DELIVERY}"
 echo ${KEY_DELIVERY} | base64 -d > key.pem
 ssh -i key.pem ec2-user@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
 scp -i key.pem package.tar.gz ec2-user@${HOST_DELIVERY}:~/minecraft/

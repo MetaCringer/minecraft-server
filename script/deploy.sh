a4485cf852eba4453e78e2e3e9f51620410366a8
metacringer <metacringer@gmail.com>
2021-09-19 02:17:15 +0300

    #/bin/bash

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 tar -pczvf package.tar.gz --exclude=".github/*" --exclude=".env" --exclude="LICENSE" --exclude="README.md" --exclude=".gitignore" --exclude="script/*" ./*
 ssh -i ~/.ssh/metacringer.pem ec2-user@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
 scp -i ~/.ssh/metacringer.pem package.tar.gz ec2-user@${HOST_DELIVERY}:~/minecraft/

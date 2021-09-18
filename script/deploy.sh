5deb14397b348a36e797b5d11c3ee4a5eb706a0f
metacringer <metacringer@gmail.com>
2021-09-19 02:28:57 +0300

    add injection of private key of ssh to deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 tar -pczvf package.tar.gz --exclude=".github/*" --exclude=".env" --exclude="LICENSE" --exclude="README.md" --exclude=".gitignore" --exclude="script/*" ./*
-ssh -i ~/.ssh/metacringer.pem ec2-user@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
-scp -i ~/.ssh/metacringer.pem package.tar.gz ec2-user@${HOST_DELIVERY}:~/minecraft/
-ssh -i ~/.ssh/metacringer.pem ec2-user@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
-rm package.tar.gz
+echo ${KEY_DELIVERY} | base64 -d > key.pem
+ssh -i key.pem ec2-user@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
+scp -i key.pem package.tar.gz ec2-user@${HOST_DELIVERY}:~/minecraft/
+ssh -i key.pem ec2-user@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
+rm package.tar.gz key.pem

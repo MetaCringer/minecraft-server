ce0aff8b7699a7df80eb61ef792a365dfb243a74
metacringer <metacringer@gmail.com>
2021-09-19 02:06:27 +0300

    added delivery to vps

---
+++ script/deploy.sh
@@ -0,0 +1,5 @@
+tar -pczvf package.tar.gz --exclude=".env" --exclude="LICENSE" --exclude="README.md" --exclude=".gitignore" --exclude="script/*" ./*
+ssh -i ~/.ssh/metacringer.pem ec2-user@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
+scp -i ~/.ssh/metacringer.pem package.tar.gz ec2-user@${HOST_DELIVERY}:~/minecraft/
+ssh -i ~/.ssh/metacringer.pem ec2-user@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
+rm package.tar.gz

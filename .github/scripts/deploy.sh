a592e06ebe0d2e9bb5922818acb6184ae92e72fc
metacringer <metacringer@gmail.com>
2021-09-19 03:04:29 +0300

    fixed names of environments

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tar -pczvf package.tar.gz --exclude=".github/*" --exclude=".env" --exclude="LICENSE" --exclude="README.md" --exclude=".gitignore" --exclude="script/*" ./*
-echo "host is ${secrets.HOST_DELIVERY}"
-echo ${secrets.KEY_DELIVERY} | base64 -d > key.pem
-ssh -i key.pem ${secrets.USER_DELIVERY}@${secrets.HOST_DELIVERY} 'mkdir -p ~/minecraft'
-scp -i key.pem package.tar.gz ec2-user@${secrets.HOST_DELIVERY}:~/minecraft/
-ssh -i key.pem ec2-user@${secrets.HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
+echo "host is ${HOST_DELIVERY}"
+echo ${KEY_DELIVERY} | base64 -d > key.pem
+ssh -i key.pem ${USER_DELIVERY}@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
+scp -i key.pem package.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:~/minecraft/
+ssh -i key.pem ${USER_DELIVERY}@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
 rm package.tar.gz key.pem

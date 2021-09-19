ea274d258ff9d5f402a13ada509ae465a3166ffb
metacringer <metacringer@gmail.com>
2021-09-19 03:28:33 +0300

    !

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -4,8 +4,8 @@ echo "host is ${HOST_DELIVERY}"
 echo ${KEY_DELIVERY} | base64 -d > key.pem
 chmod 600 key.pem
 mkdir ~/.ssh/
-ssh-keyscan -H ec2-54-93-59-82.eu-central-1.compute.amazonaws.com > ~/.ssh/known_hosts
-ssh -i key.pem ${USER_DELIVERY}@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
-scp -i key.pem package.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:~/minecraft/
-ssh -i key.pem ${USER_DELIVERY}@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
+ssh-keyscan -H ${HOST_DELIVERY} > ~/.ssh/known_hosts
+ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} 'mkdir -p ~/minecraft'
+scp -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts package.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:~/minecraft/
+ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} 'cd minecraft && tar -xvf ~/minecraft/package.tar.gz && rm -f ~/minecraft/package.tar.gz'
 rm package.tar.gz key.pem

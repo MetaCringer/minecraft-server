a82ffebb8257514e7fe9d39f621518d011c7a0bf
metacringer <metacringer@gmail.com>
2021-09-20 00:25:54 +0300

    changed path of delivery

--- .github/scripts/deliver-mods.sh
+++ .github/scripts/deliver-mods.sh
@@ -6,6 +6,6 @@ chmod 600 key.pem
 mkdir ~/.ssh/
 ssh-keyscan -H ${HOST_DELIVERY} > ~/.ssh/known_hosts
 ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} 'mkdir -p ~/minecraft/mods/'
-scp -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts mods.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:~/minecraft/mods/
+scp -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts mods.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:~/minecraft/
 ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} 'cd minecraft/mods && tar -xvf ~/minecraft/mods.tar.gz && rm -f ~/minecraft/mods.tar.gz'
 rm -f mods.tar.gz key.pem

abec398b8892a88f2ee3630a30704eb6fcf19765
metacringer <metacringer@gmail.com>
2021-12-11 16:11:01 +0200

    debug echo was added

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -9,7 +9,7 @@ echo "i am $(whoami)"
 ssh-keyscan -H ${HOST_DELIVERY} > ~/.ssh/known_hosts
 echo "known host is"
 cat ~/.ssh/known_hosts
-echo "----"
+echo "---- ${PATH_DELIVERY}"
 ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} "mkdir -p ${PATH_DELIVERY}/"
 scp -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts package.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:${PATH_DELIVERY}/
 ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} "cd ${PATH_DELIVERY}/ && tar -xvf ${PATH_DELIVERY}/package.tar.gz && rm -f ${PATH_DELIVERY}/package.tar.gz"

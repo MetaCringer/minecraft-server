f36cd0c4f172d0248ecaf8bc5670ab6e8bc1eca7
metacringer <metacringer@gmail.com>
2021-12-11 16:02:10 +0200

    debug echo was added

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,6 +5,7 @@ echo ${KEY_DELIVERY} | base64 -d > key.pem
 chmod 600 key.pem
 mkdir ~/.ssh/
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "i am $(whoami)"
 ssh-keyscan -H ${HOST_DELIVERY} > ~/.ssh/known_hosts
 ssh -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts ${USER_DELIVERY}@${HOST_DELIVERY} "mkdir -p ${PATH_DELIVERY}/"
 scp -i key.pem -o UserKnownHostsFile=~/.ssh/known_hosts package.tar.gz ${USER_DELIVERY}@${HOST_DELIVERY}:${PATH_DELIVERY}/

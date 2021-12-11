db6421c582a4aa6b93d36baf1b13f63583457acd
metacringer <metacringer@gmail.com>
2021-12-11 16:53:33 +0200

    Added delivering mods

--- .github/scripts/deliver-mods.sh
+++ .github/scripts/deliver-mods.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd mods && tar -pczvf ../mods.tar.gz ./* && cd ../
+cd mods && tar -pczvf ../mods.tar.gz ./ && cd ../
 echo "host is ${HOST_DELIVERY}"
 echo ${KEY_DELIVERY} | base64 -d > key.pem
 chmod 600 key.pem
